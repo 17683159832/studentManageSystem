ab004386adcc318364657945f239e88672894785
dengxiande <1376618389@qq.com>
2021-06-15 20:18:19 +0800

    开发文件

--- src/AbstractMain.java
+++ src/AbstractMain.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractMain {
+/**
+ *  其他接口 实现  必须  继承此类
+ *
+ *  该类的功能主要负责：临时保存学生， 负责io转为文件保存
+ */
+
+
+    //临时存放学生的集合,,所有需要增加，修改的东西，需要使用该集合来访问该学生
+    List<Student> studentList = new ArrayList<>(20);
+
 
 }
