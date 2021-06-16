faacf6f86fa9fbff253c1b0e3b160ca7155e3703
zzaislxcdbb1000 <2465128506@qq.com>
2021-06-16 11:15:37 +0800

    1

---
+++ src/show.java
@@ -0,0 +1,59 @@
+public class show implements ShowStudentScore {
+
+
+
+
+    //展示全部学生
+    @Override
+    public void showAllStu() {
+
+        for (int i =0;i<studentList.size();i++){
+
+            System.out.println("名字："+studentList.get(i).getName()+" "+"班级："+studentList.get(i).get_class()+
+                    " "+"学号："+studentList.get(i).getId()+" "+"语文成绩："+studentList.get(i).getChinese()+" "+"数学成绩:"
+            +studentList.get(i).getMath()+" "+"英语成绩"+studentList.get(i).getEnglish());
+
+        }
+    }
+    //按语文降序展示
+    @Override
+    public void showDescChinese() {
+        for (int i=0;i<studentList.size();i++){
+            for (int j=0;j<studentList.size()-i;j++){
+                if (studentList.get(j).getChinese()<studentList.get(j+1).getChinese()){
+                    double t = 0;
+                    t  = studentList.get(j).getChinese();
+                    studentList.get(j).getChinese()=studentList.get(j+1).getChinese();
+                    studentList.get(j+1).getChinese()=t;
+                }
+
+            }
+        }
+        for (int i = 0;i<studentList.size();i++){
+            System.out.println("名字："+studentList.get(i).getName()+" "+"班级："+studentList.get(i).get_class()+
+                    " "+"学号："+studentList.get(i).getId()+" "+"语文成绩："+studentList.get(i).getChinese()+" "+"数学成绩:"
+                    +studentList.get(i).getMath()+" "+"英语成绩"+studentList.get(i).getEnglish());
+        }
+
+    }
+    //通过参数班级名，按班级展示
+    @Override
+    public void showStuByClass(String _className) {
+
+        System.out.println("一下都是"+_className+"的");
+        for (int i = 0;i<studentList.size();i++){
+            if (studentList.get(i).get_class() = _className){
+                System.out.println("名字："+studentList.get(i).getName()+" "+"班级："+studentList.get(i).get_class()+
+                        " "+"学号："+studentList.get(i).getId()+" "+"语文成绩："+studentList.get(i).getChinese()+" "+"数学成绩:"
+                        +studentList.get(i).getMath()+" "+"英语成绩"+studentList.get(i).getEnglish());
+            }
+        }
+
+
+    }
+
+
+
+
+
+}
