ec4d90163c1267c3e1690bcacfc53443abcb299f
zrz-0405 <752675311@qq.com>
2021-06-16 16:06:59 +0800

    3

--- src/StuIdStore.java
+++ src/StuIdStore.java
@@ -3,8 +3,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class StuIdStore  implements AddAndUpdateScore{
 
-    private Object StudentID;
-
     @Override
     public boolean addScore(int id){
 
@@ -12,49 +10,33 @@ public class StuIdStore  implements AddAndUpdateScore{
         for (int i = 0; i < StuSystem.studentList.size(); i++) {
             if(studentList.get(i).getId()==id){
              Student s=studentList.get(i);
-               if(s.getChinese()!=null){
-                   Scanner scanner = new Scanner(System.in);
+                Scanner scanner = new Scanner(System.in);
+               if(s.getChinese()==null) {
                    System.out.println("输入语文成绩：");
                    double C = scanner.nextDouble();
-
+                   s.setChinese(C);
+               }else{
+                   System.out.println("已有语文成绩不用输入");
                }
-            }else{
-                System.out.println( "找不到该学号！");
-                return  false;
-
-            }
-        }
-        for (int i = 0; i < StuSystem.studentList.size(); i++) {
-            if(studentList.get(i).getId()==id){
-                Student s=studentList.get(i);
-                if(s.getChinese()!=null){
-                    Scanner scanner = new Scanner(System.in);
-                    System.out.println("输入数学成绩：");
-                    double M = scanner.nextDouble();
-
+                if(s.getMath()==null){
+                   System.out.println("输入数学成绩：");
+                   double M = scanner.nextDouble();
+                   s.setMath(M);
+                }else{
+                    System.out.println("已有数学成绩不用输入");
                 }
-            }else{
-                System.out.println( "找不到该学号！");
-                return  false;
+                if(s.getEnglish()==null){
+                   System.out.println("输入英语成绩：");
+                   double E = scanner.nextDouble();
+                   s.setEnglish(E);
 
-            }
-        }
-        for (int i = 0; i < StuSystem.studentList.size(); i++) {
-            if(studentList.get(i).getId()==id){
-                Student s=studentList.get(i);
-                if(s.getChinese()!=null){
-                    Scanner scanner = new Scanner(System.in);
-                    System.out.println("输入英语成绩：");
-                    double E = scanner.nextDouble();
-
+               }else{
+                    System.out.println("已有英语成绩不用输入");
                 }
-            }else{
-                System.out.println( "找不到该学号！");
-                return  false;
-
+                return  true;
             }
         }
-
+        System.out.println( "找不到该学号！");
         return false;
     }
 
@@ -64,22 +46,26 @@ public class StuIdStore  implements AddAndUpdateScore{
             if(studentList.get(i).getId()==id){
                 Student s=studentList.get(i);
                 if(s.getChinese()!=null){
+
                     Scanner scanner = new Scanner(System.in);
+
+                    System.out.println("原来的语文成绩为"+s.getChinese()+"更改语文成绩:");
+
                     double C = scanner.nextDouble();
-                    System.out.println("更改语文成绩"+C);
+                    s.setChinese(C);
+                    System.out.println("原来的数学成绩为"+s.getMath()+"更改数学成绩:");
+                    double M= scanner.nextDouble();
+                    s.setMath(M);
+                    System.out.println("原来的英语成绩为"+s.getEnglish()+"更改英语成绩:");
+                    double E = scanner.nextDouble();
+                    s.setEnglish(E);
+
 
                 }
-            }else{
-                System.out.println( "找不到该学号！");
-                return  false;
-
             }
         }
+        System.out.println( "更改成功");
         return false;
     }
 }
 
-class StudentID implements StuSystem { }
-class chinese extends Student{ }
-class math extends  Student  {  }
-class english extends Student{ }
