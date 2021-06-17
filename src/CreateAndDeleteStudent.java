b79958e1d9c63e3374b0de9aed2d674fd040cd5c
lxc201014 <1812238573@qq.com>
2021-06-17 11:00:55 +0800

    帅逼

--- src/CreateAndDeleteStudent.java
+++ src/CreateAndDeleteStudent.java
@@ -6,10 +6,10 @@
 public interface CreateAndDeleteStudent extends StuSystem{
 
     //创建学生（无成绩），并把学生存放在studentList中
-    boolean createStu();
+    Student createStu();
 
-    //创建学生（同时添加成绩），并把学生存放在studentList中
-    boolean createStuAndScore();
+   //修改学生基本信息
+    void updateStu(int id);
 
     //通过学号删除学生
     boolean deleteStuById(int id);
--- src/CreateStudent.java
+++ src/CreateStudent.java
@@ -4,31 +4,36 @@ public class CreateStudent implements CreateAndDeleteStudent{
 
     //创建学生（无成绩），并把学生存放在studentList中
     @Override
-    public boolean createStu() {
+    public Student createStu() {
+        System.out.println("输入学生姓名：");
         Scanner can = new Scanner(System.in);
         String name = can.nextLine();
         Student s = new Student();
         s.setName(name);
+        System.out.println("输入学生班级：");
+        s.set_class(can.nextLine());
+        System.out.println("输入学生学号：");
+        s.setId(can.nextInt());
+        System.out.println("输入语文成绩：");
+        s.setChinese(can.nextDouble());
+        System.out.println("输入数学成绩：");
+        s.setMath(can.nextDouble());
+        System.out.println("输入英语成绩：");
+        s.setEnglish(can.nextDouble());
         studentList.add(s);
-        return false;
+        return s;
     }
 
-    //创建学生（同时添加成绩），并把学生存放在studentList中
+    //修改学生基本信息
     @Override
-    public boolean createStuAndScore() {
+    void updateStu(int id) {
         createStu();
         Scanner scanner = new Scanner(System.in);
-        double great = scanner.nextDouble();
-        Scanner scanner1 = new Scanner(System.in);
-        double great1 = scanner1.nextDouble();
-        Scanner scanner2 = new Scanner(System.in);
-        double great2 = scanner2.nextDouble();
         Student student = new Student();
-        student.setMath(great);
-        student.setChinese(great1);
-        student.setEnglish(great2);
+        student.setName(scanner.nextLine());
+        student.set_class(scanner.nextLine());
+        student.setId(scanner.nextInt());
         studentList.add(student);
-        return false;
     }
 
     //通过学号删除学生
